c86c050d79aafc5a80ff13a3bbe5a211ae9af356
Alif <m.alif.fadhillah32@gmail.com>
2024-03-01 10:51:49 +0800

    build: Add CORS middleware to handle cross-origin requests

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const dbPool = mysql.createPool({
-  port: process.env.PORTSQL,
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const mysql2 = require("mysql2");
 const middlewareLogRequest = require("./middleware/logs.js");
 const routesTag = require("./routes/tanaman.js");
+const routesUser = require("./routes/user.js");
+const cors = require("cors");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*", // Izinkan permintaan dari semua sumber
+  })
+);
 
 app.use(middlewareLogRequest);
 app.use(express.json());
